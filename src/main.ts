0d76f08c4de9e229429126841a653e92e8db403a
sasha <neveisya@yandex.ru>
2025-06-09 06:57:03 +0300

    Установить PrimeVue

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
-import './assets/main.css'
-
+import './assets/base.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
 
 const app = createApp(App)
 
 app.use(createPinia())
-
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+})
 app.mount('#app')
